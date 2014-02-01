b63699694c0a5cb43675014cde01d64b5dc0098a
Mohamed Eltuhamy <tuhamy@gmail.com>
2014-02-01 19:24:25 +0000

    Finished basic implementation

--- README.md
+++ README.md
@@ -7,9 +7,28 @@ Usage
 -----
 
 1. You will need [node.js](http://nodejs.org/download/)
-2. Copy ```watch.js``` and ```package.json``` into your latex project folder.
+2. Copy ```watch.js``` and ```package.json``` into your latex project folder, where your main ```.tex``` file is.
 3. In your latex project folder, install node dependencies by running ```npm install```.
-4. Start the watcher script by running ```npm start```
+4. Start the watcher script by running ```node watch.js```. Check options below:
+
+Options
+-------
+
+    -c, --command  list of commands to run. e.g latex,bibtex,pdflatex  [required]  [default: "pdflatex"]
+    -t, --tex      tex file to compile on changes                      [required]
+    -b, --bib      bib file used (required if bibtex is used)        
+
+Example
+-------
+The command below compiles the ```main.tex``` file with latex, then bibtex (using ```main.bib```), then latex again, then pdflatex every time any .tex files are changed, added or removed.
+
+    ❯ node watch.js -c latex,bibtex,latex,pdflatex -t main -b main
+    Watching
+    latex ✓
+    bibtex ✓
+    latex ✓
+    pdflatex ✓
+
 
 Notes
 -----
--- watch.js
+++ watch.js
@@ -1,53 +1,102 @@
 var gaze = require('gaze'),
     sys   = require('sys'),
     spawn = require('child_process').spawn,
+    argv = require('optimist')
+           .usage('Watch latex files and compile them.\nUsage: $0')
+           .demand('c')
+           .alias('c', 'command')
+           .describe('c', 'list of commands to run. e.g latex,bibtex,pdflatex')
+           .demand('t')
+           .alias('t', 'tex')
+           .describe('t', 'tex file to compile on changes')
+           .alias('b','bib')
+           .describe('b', 'bib file used (required if bibtex is used)')
+           .default('c', 'pdflatex').argv,
+    texName = argv.t,
+    bibName = argv.bib,
+    commands = argv.c,
 
     compileBibtex = function(cb){
-      var bibtex      = spawn('bibtex', ['main']);
+      process.stdout.write('bibtex ');
+      var bibtex      = spawn('bibtex', [bibName]);
       bibtex.on('exit', function (code) {
-        if(code != 0){
-          console.error("bibtex exited with error " + code);
-        }
+        process.stdout.write((code==0 ? '✓' : '×') + '\n');
         if(cb != undefined) cb();
       });
     },
 
-    displayErrors = function(){
+    displayErrors = function(cb){
       //grep -n -A 1 ^! main.log
-      var grep = spawn('grep', ['-n', '-A', '1', '^!', 'main.log']);
+      var logfilename = texName.split("/").pop().split('.')[0];
+      var grep = spawn('grep', ['-n', '-A', '1', '^!', logfilename+'.log']);
       grep.stdout.pipe(process.stdout);
+      grep.on('exit', function(code){
+        if(cb!=undefined) cb();
+      });
     },
 
     compileLatex = function(cb){
-      var pdflatex      = spawn('pdflatex', ['-interaction=nonstopmode','main']);
-      pdflatex.on('exit', function (code) {
+      process.stdout.write('latex ');
+      var latex      = spawn('latex', ['-interaction=nonstopmode',texName]);
+      latex.on('exit', function (code) {
+        process.stdout.write((code==0 ? '✓' : '×') + '\n');
         if(code != 0){
-          console.error("pdflatex exited with error " + code);
-          displayErrors();
+          displayErrors(cb);
         } else if(cb != undefined) cb();
       });
     },
 
-    compileAll = function(cb){
-      console.log("Compiling...");
-      compileLatex(function(){
-        compileBibtex(function(){
-          compileLatex(function(){
-            compileLatex(function(){
-              console.log("Done.");
-            });
-          });
-        });
+    compilePDFLatex = function(cb){
+      process.stdout.write('pdflatex ');
+      var pdflatex      = spawn('pdflatex', ['-interaction=nonstopmode',texName]);
+      pdflatex.on('exit', function (code) {
+        process.stdout.write((code==0 ? '✓' : '×') + '\n');
+        if(code != 0){
+          displayErrors(cb);
+        } else if(cb != undefined) cb();
       });
-    };
+    },
 
+    getCommandChain = function(csvCommands, start, end){
+      var commands = csvCommands.split(',');
+
+
+      var getNextCallback = function(i){
+        // console.log("Genenerating "+i+": "+commands[i]);
+        if(i < commands.length){
+          if(commands[i] === 'latex'){
+            return function(){
+              compileLatex(getNextCallback(i+1));
+            };
+          } else if (commands[i] === 'pdflatex') {
+            return function(){
+              compilePDFLatex(getNextCallback(i+1));
+            };
+          } else if (commands[i] === 'bibtex') {
+            return function(){
+              compileBibtex(getNextCallback(i+1));
+            };
+          }
+        } else {
+          // do this in the NEXT callback!
+          if(end!=undefined) end();
+        }
+      };
+
+      return function(){
+        if(start!=undefined) start();
+        getNextCallback(0)();
+      };
+    },
+
+    compileAll = getCommandChain(commands);
+
+console.log("Watching");
 // compile first now.
 compileAll();
 
 // Watch all .tex files/dirs in process.cwd()
 gaze(['**/*.tex', '**/*.bib'], function(err, watcher) {
-
-  console.log("Watching")
 
   // On changed/added/deleted
   this.on('all', function(event, filepath) {
